2426a335eafc9e51b900dbe003f1a93a18e6f99a
Filza Mazahir <filza.mazahir@gmail.com>
2016-01-20 17:27:02 -0800

    player names saved

--- shootingballz_mean/server.js
+++ shootingballz_mean/server.js
@@ -11,21 +11,36 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
- res.render("index");
+    res.render("index");
 });
 
+var players = []
+
 var server = app.listen(5000, function() {
- console.log("listening on port 5000");
+    console.log("listening on port 5000");
 });
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
-  console.log("SERVER::WE ARE USING SOCKETS!");
-  console.log(socket.id);
+
+
+    console.log("SERVER::WE ARE USING SOCKETS!");
+    console.log(socket.id);
+    // socket.emit("allPlayers", players)
+    
 
     socket.on("newUserConnected", function(data) {
       console.log("user joined in iOS", data);
-      io.sockets.emit("newUserJoinedServer", data);
+      players.push(data)
+      console.log("Players as of now: ", players);
+      // io.sockets.emit("newUserJoinedServer", data);
+      io.sockets.emit("updatePlayers", players[0], players[1], players[2], players[3]);
     });
+
+    // socket.on('disconnect', function() {
+    //     console.log('Got disconnect!');
+    //     players = []
+    // });
+
 
 });
